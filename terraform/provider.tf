5cb502a73321ef1ac32e4ad7565a7b6e8e466272
ncorazza12 <rm562265@fiap.com.br>
2025-08-20 22:01:21 -0300

    Configura backend S3 e pipeline Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,12 @@
-# PROVIDER
 terraform {
-
   required_version = "~> 1.12.2"
+
+  backend "s3" {
+    bucket         = "aws-notifier-nickolas"  # nome do bucket que você vai criar
+    key            = "tfstate/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
@@ -9,12 +14,8 @@ terraform {
       version = "~> 6.9.0"
     }
   }
-
 }
 
 provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "iac"
+  region = "us-east-1"
 }
